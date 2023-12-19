173068c6030712c057cf2fd0eb951ed4d8f85d04
MeninoFranca <baueremil9@gmail.com>
2023-12-19 12:00:23 -0300

    Armazendo e exibindo values (no console)

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function subscribe() {
+    let inputname = document.querySelector("#name");
+    let name = inputname.value;
+    let inputemail = document.querySelector("#email");
+    let email = inputemail.value;
+    let inputestado = document.querySelector("#estado");
+    let estado = inputestado.value;
+    console.log("Nome: "+name);
+    console.log("Email: "+email);
+    console.log("Estado: "+estado);
+}
