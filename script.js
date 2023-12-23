c5a72d31770cc777a498e31ada77be28204520e7
MeninoFranca <baueremil9@gmail.com>
2023-12-23 15:01:43 -0300

    Adicionando integração com banco de dados(No sql)

--- script.js
+++ script.js
@@ -9,4 +9,18 @@ function subscribe() {
     console.log("Nome: "+name);
     console.log("Email: "+email);
     console.log("Estado: "+estado);
+
+const Config = {
+    apiKey: "AIzaSyD14v4w5V9Sq2Q6JzptqM8oZq8ToaHwH_A",
+    authDomain: "pagina-de-captura-c23c0.firebaseapp.com",
+    projectId: "pagina-de-captura-c23c0",
+    storageBucket: "pagina-de-captura-c23c0.appspot.com",
+    messagingSenderId: "479616534005",
+    appId: "1:479616534005:web:b7065f2f63758c0e254cbc",
+    measurementId: "G-MEGJ28X7MB"
+};
+firebase.initializeApp(Config);
+var db = firebase.firestore();
+
 }
+
