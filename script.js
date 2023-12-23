4e5e8118c65d977b8a48d9c2e6b5c172a14388f8
MeninoFranca <baueremil9@gmail.com>
2023-12-23 15:46:37 -0300

    Adicionando dados ao banco de dados

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-
 function subscribe() {
     let inputname = document.querySelector("#name");
-    let name = inputname.value;
+    let namep = inputname.value;
     let inputemail = document.querySelector("#email");
-    let email = inputemail.value;
+    let emailp = inputemail.value;
     let inputestado = document.querySelector("#estado");
-    let estado = inputestado.value;
-    console.log("Nome: "+name);
-    console.log("Email: "+email);
-    console.log("Estado: "+estado);
+    let estadop = inputestado.value;
+    console.log("Nome: " + namep);
+    console.log("Email: " + emailp);
+    console.log("Estado: " + estadop);
 
-const Config = {
-    apiKey: "AIzaSyD14v4w5V9Sq2Q6JzptqM8oZq8ToaHwH_A",
-    authDomain: "pagina-de-captura-c23c0.firebaseapp.com",
-    projectId: "pagina-de-captura-c23c0",
-    storageBucket: "pagina-de-captura-c23c0.appspot.com",
-    messagingSenderId: "479616534005",
-    appId: "1:479616534005:web:b7065f2f63758c0e254cbc",
-    measurementId: "G-MEGJ28X7MB"
-};
-firebase.initializeApp(Config);
-var db = firebase.firestore();
-
+    const captura = "Captura";
+    const Config = {
+        apiKey: "AIzaSyD14v4w5V9Sq2Q6JzptqM8oZq8ToaHwH_A",
+        authDomain: "pagina-de-captura-c23c0.firebaseapp.com",
+        projectId: "pagina-de-captura-c23c0",
+        storageBucket: "pagina-de-captura-c23c0.appspot.com",
+        messagingSenderId: "479616534005",
+        appId: "1:479616534005:web:b7065f2f63758c0e254cbc",
+        measurementId: "G-MEGJ28X7MB"
+    };
+    firebase.initializeApp(Config);
+    var db = firebase.firestore();
+    db.collection(captura).add({
+        nome: namep,
+        email: emailp,
+        estado: estadop,
+    }).then((doc) => {
+        console.log("Documento inserido no banco de dados com sucesso");
+    }).catch(err => {
+        console.log(err);
+    });
 }
-
